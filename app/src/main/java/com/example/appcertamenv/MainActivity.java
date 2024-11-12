549425a44cf1adb3188b2dd0e7bd1e5daf3c8929
Work <udecamtres@gmail.com>
2024-11-12 15:03:46 -0300

    * Implementación intermedia de la funcion creada de calculo IMC

--- app/src/main/java/com/example/appcertamenv/MainActivity.java
+++ app/src/main/java/com/example/appcertamenv/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        peso = findViewById(R.id.);
+        peso = findViewById(R.id.txtPeso);
         estatura = findViewById(R.id.txtEstatura);
         sp = findViewById(R.id.spinnerrr);
         caja = findViewById(R.id.txtCaja);
